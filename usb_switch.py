c5070a8e23f71151ab251d009f95e3b243ddea16
maxla <maxlavoie1960@hotmail.com>
2024-09-30 18:16:34 -0400

    updated pin number

--- usb_switch.py
+++ usb_switch.py
@@ -1,11 +1,11 @@
 import gpiod
 
-USB_PIN = 4
+USB_PIN = 7
 
 chip = gpiod.Chip('/dev/gpiochip0')
-led_line = chip.get_line(USB_PIN)
-led_line.request(consumer="USB switch", type=gpiod.LINE_REQ_DIR_OUT)
-led_line.set_value(1)
+usb_line = chip.get_line(USB_PIN)
+usb_line.request(consumer="USB switch", type=gpiod.LINE_REQ_DIR_OUT)
+usb_line.set_value(1)
 input('Switch?')
-led_line.set_value(0)
-led_line.release()
+usb_line.set_value(0)
+usb_line.release()
