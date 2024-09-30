11b4f074733d1a2d616c54ba0eefafe9393ebb3c
maxla <maxlavoie1960@hotmail.com>
2024-09-30 18:13:22 -0400

    created usb switch script

---
+++ usb_switch.py
@@ -0,0 +1,11 @@
+import gpiod
+
+USB_PIN = 4
+
+chip = gpiod.Chip('gpiochip0')
+led_line = chip.get_line(USB_PIN)
+led_line.request(consumer="USB switch", type=gpiod.LINE_REQ_DIR_OUT)
+led_line.set_value(1)
+input('Switch?')
+led_line.set_value(0)
+led_line.release()
