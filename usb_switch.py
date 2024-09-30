8a4fcdad53c14934047e530bdcae8d4abb59d632
maxla <maxlavoie1960@hotmail.com>
2024-09-30 18:15:02 -0400

    updated board path

--- usb_switch.py
+++ usb_switch.py
@@ -2,7 +2,7 @@ import gpiod
 
 USB_PIN = 4
 
-chip = gpiod.Chip('gpiochip0')
+chip = gpiod.Chip('/dev/gpiochip0')
 led_line = chip.get_line(USB_PIN)
 led_line.request(consumer="USB switch", type=gpiod.LINE_REQ_DIR_OUT)
 led_line.set_value(1)
