89ffd0203a6dc404bf3c36716ba2252eab35efd2
maxla <maxlavoie1960@hotmail.com>
2024-10-02 22:57:50 -0400

    changed pins

--- usb_switch.py
+++ usb_switch.py
@@ -2,15 +2,15 @@ import gpiod
 
 from gpiod.line import Direction, Value
 
-USB_POWER_NC = 17
-USB_DATA_NO = 27
+USB_POWER_NC = 5
+USB_DATA_NO = 6
 
 with gpiod.request_lines(
     "/dev/gpiochip0",
     consumer="blink-example",
     config={
-        USB_POWER_NC: gpiod.LineSettings(direction=Direction.OUTPUT, output_value=Value.ACTIVE),
-        USB_DATA_NO: gpiod.LineSettings(direction=Direction.OUTPUT, output_value=Value.ACTIVE),
+        USB_POWER_NC: gpiod.LineSettings(direction=Direction.OUTPUT, output_value=Value.INACTIVE),
+        USB_DATA_NO: gpiod.LineSettings(direction=Direction.OUTPUT, output_value=Value.INACTIVE),
     },
 ) as request:
     input("Turn on data ")
