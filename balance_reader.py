94807a21e4e41cc2c446b317a803a35a1a8a3d37
maxla <maxlavoie1960@hotmail.com>
2024-10-10 20:44:16 -0400

    print stable weight

--- balance_reader.py
+++ balance_reader.py
@@ -2,7 +2,13 @@ import serial
 
 with serial.Serial("/dev/ttyUSB0", 1200) as serial_communication:
   serial_communication.write(b"?\r\n")
+  last_weight = 0
   while True:
     reading = serial_communication.readline().decode()
     reading_segments = list(filter(None, reading.strip().split(' ')))
-    print(reading_segments)
+    _, info, weight_string, unit = reading_segments
+    weight = float(weight_string)
+    is_stable = '*' not in info
+    if is_stable and last_weight != weight:
+      print(weight, unit)
+      last_weight = weight
