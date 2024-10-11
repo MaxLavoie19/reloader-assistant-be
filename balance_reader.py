bed8fbc24d45a7cc7ec7d0ba0ea7c0eca1d01878
maxla <maxlavoie1960@hotmail.com>
2024-10-10 20:30:46 -0400

    remove blanks

--- balance_reader.py
+++ balance_reader.py
@@ -4,5 +4,5 @@ with serial.Serial("/dev/ttyUSB0", 1200) as serial_communication:
   serial_communication.write(b"?\r\n")
   while True:
     reading = serial_communication.readline().decode()
-    reading_segments = reading.strip().split(' ')
+    reading_segments = list(filter(None, reading.strip().split(' ')))
     print(reading_segments)
