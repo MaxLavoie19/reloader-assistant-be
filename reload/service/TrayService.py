6c2a0c1849bfdb812bb502cd2ba5bd63914c7f6b
maxla <maxlavoie1960@hotmail.com>
2024-10-21 23:47:24 -0400

    ordered coordinates and components for better bagging

--- reload/service/TrayService.py
+++ reload/service/TrayService.py
@@ -14,7 +14,8 @@ class TrayService:
     return coordinates
 
   def print_bin(self, prefix: str, bin: List):
-    index_arrays, values_arrays = np.split(np.array(bin), 2, axis=1)
+    sorted_bin = sorted(bin)
+    index_arrays, values_arrays = np.split(np.array(sorted_bin), 2, axis=1)
     indexes = index_arrays.flatten()
     values = values_arrays.flatten()
     coordinates = list(map(lambda x: self.get_coordinates(x), indexes))
